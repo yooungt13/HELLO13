c6afa7d1561c875cb9a4ce64d7799ba7bdc05a25
yooungt13 <61123072@qq.com>
2014-03-06 21:56:22 +0800

    baidu tongji

--- public/javascripts/plugins.js
+++ public/javascripts/plugins.js
@@ -57,3 +57,7 @@ $('#projects').hover(function() {}, function() {
     isproj = false;
   }
 });
+
+// 百度统计
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F78bbe26f8e0ea4154a3ab176ed98aa8f' type='text/javascript'%3E%3C/script%3E"));

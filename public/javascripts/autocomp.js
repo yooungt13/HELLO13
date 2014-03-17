5986535104348084c24f2e38395db21340cb7492
yooungt13 <61123072@qq.com>
2014-03-17 23:11:24 +0800

    add autocomp

---
+++ public/javascripts/autocomp.js
@@ -0,0 +1,133 @@
+var $$ = function(id) {
+	return document.getElementById(id);
+}
+
+var addEvent = function(o, e, f) {
+	o.addEventListener ? o.addEventListener(e, f, false) : o.attachEvent('on' + e, function() {
+		f.call(o)
+	});
+}
+
+var Input = function() {};
+Input.prototype = {
+	input: null,
+	options: ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+		'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+		'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+		'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+		'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+		'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+		'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+		'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+		'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+	],
+
+	inputX: 0,
+	inputY: 0,
+	inputWidth: 300,
+	inputHeight: 30,
+
+	init: function(config) {
+		this.input = $$('search');
+
+		if (config) {
+			this.options = config.options || this.options;
+			this.inputWidth = config.width || this.inputWidth;
+			this.inputHeight = config.height || this.inputHeight;
+		}
+
+		this.clearScreen();
+		this.setStyle();
+
+		return this;
+	},
+	setStyle: function() {
+		this.input.style.width = this.inputWidth + 'px';
+		this.input.style.height = this.inputHeight + 'px';
+		this.input.style.padding = '8px 12px';
+		this.input.style.fontSize = '24px';
+		this.input.style.lineHeight = '30px';
+		this.input.style.border = '2px solid #ccc';
+		this.input.style.borderRadius = '5px';
+		this.input.style.outline = 'none';
+	},
+	autoComplete: function() {
+		var wrap = $$('wrap'),
+			that = this;
+
+		addEvent(this.input, 'input', function() {
+			that.clearScreen();
+
+			if (this.value) { // 若input值不为空，则进行匹配
+				var pattern = new RegExp("^" + this.value + "", "i");
+				var str = [];
+
+				for (var i = 0, len = that.options.length; i < len; i++) {
+					if (pattern.test(that.options[i]))
+						str.push(that.options[i]);
+				}
+
+				if (str.length > 0) {
+					var ul = document.createElement('div');
+					ul.id = 'list';
+					ul.style.position = 'absolute';
+					ul.style.width = that.inputWidth + 25 + 'px';
+					ul.style.listStyleType = 'none';
+					ul.style.border = '1px solid #ccc';
+					//ul.style.boxShadow = '2px 2px 5px #000';
+					ul.style.lineHeight = '30px';
+					ul.style.color = '#aaa';
+					ul.style.top = this.offsetTop + that.inputHeight + 22 + 'px';
+					ul.style.left = this.offsetLeft + 'px';
+					ul.style.textAlign = 'left';
+					ul.style.textIndent = '8px';
+					ul.style.backgroundColor = '#fff';
+
+					var _this = this;
+					for (var i = 0, len = str.length; i < len; i++) {
+						var li = document.createElement('div');
+
+						li.innerHTML = str[i];
+						(function() {
+							var p = i;
+							addEvent(li, 'mousedown', function() {	
+								_this.value = str[p];
+								wrap.removeChild($$('list'));
+							});
+
+							addEvent(li, 'mouseover', function() {
+								this.style.backgroundColor = '#eee';
+							});
+
+							addEvent(li, 'mouseout', function() {
+								this.style.backgroundColor = '#fff';
+							});
+						})();
+
+						ul.appendChild(li);
+					}
+					wrap.appendChild(ul);
+				}
+			}
+		});
+
+		addEvent(this.input, 'focus', function() {
+			this.style.borderColor = '#000';
+		});
+
+		addEvent(this.input, 'blur', function() {
+			this.style.borderColor = '#ccc';
+		});
+
+		addEvent(window, 'mousedown', function() {
+			that.clearScreen();
+		})
+	},
+	clearScreen: function() {
+		var list = $$('list');
+		// 清除之前的选项框
+		if ( !! list) wrap.removeChild(list);
+	}
+};
+
+window.onload = new Input().init().autoComplete();
